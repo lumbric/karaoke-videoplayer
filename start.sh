1283c9d2362ae469ad4794f90303eb4eb2e280ca
lumbric <lumbric@gmail.com>
2025-07-27 22:16:31 +0200

    Add script to start browser and webserver

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Starte locale web server - necessary for video streaming
+cd "$(dirname "$0")"
+python3 -m http.server 8000 &
+
+sleep 2
+
+# Start browser in kiosk mode
+if command -v chromium >/dev/null; then
+  chromium --kiosk http://localhost:8000
+elif command -v firefox >/dev/null; then
+  firefox --kiosk http://localhost:8000
+else
+  echo "Neither firefox nor chromium found!"
+  exit 1
+fi
