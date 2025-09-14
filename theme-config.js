22333e23e9459653049cc350f8cb3ad6af5d63b8
lumbric <lumbric@gmail.com>
2025-09-14 19:26:56 +0200

    Revert "Add Neon Theme CSS with cyberpunk style, including custom fonts, animations, and responsive design"
    This reverts commit f22632da1a475d486f763d78eef86f07044d368c.

--- theme-config.js
+++ theme-config.js
@@ -1,6 +1,6 @@
-glaub // Theme Configuration
+// Theme Configuration
 // Change the THEME_NAME to switch between different themes
-// Available themes: 'karaoke-ab-hof', 'dark-theme', 'neon-theme', 'birthday-party'
+// Available themes: 'karaoke-ab-hof', 'dark-theme', 'neon-theme'
 
 const THEME_CONFIG = {
   // Set the theme folder name here
@@ -11,19 +11,19 @@ const THEME_CONFIG = {
   
   // Theme paths (automatically calculated based on THEME_NAME)
   get CSS_PATH() {
-    return `themes/${this.THEME_NAME}/theme.css`;
+    return `static/${this.THEME_NAME}/theme.css`;
   },
   
   get LOGO_PATH() {
-    return `themes/${this.THEME_NAME}/logo.png`;
+    return `static/${this.THEME_NAME}/logo.png`;
   },
   
   get COVER_FALLBACK_PATH() {
-    return `themes/${this.THEME_NAME}/cover_fallback.svg`;
+    return `static/${this.THEME_NAME}/cover_fallback.svg`;
   },
   
   // Helper function to get any theme asset
   getAssetPath(filename) {
-    return `themes/${this.THEME_NAME}/${filename}`;
+    return `static/${this.THEME_NAME}/${filename}`;
   }
 };
