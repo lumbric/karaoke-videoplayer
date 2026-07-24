ba023ebf6687e376518451e67ed90e32897c3bef
lumbric <lumbric@gmail.com>
2026-07-24 20:01:03 +0200

    Add comment with hint for YouTube API

--- secret-config.example.js
+++ secret-config.example.js
@@ -1,4 +1,5 @@
 // Copy this file to secret-config.js and set your private keys there. Do NOT commit secret-config.js.
 window.SECRETS = {
+  // https://developers.google.com/youtube/v3/getting-started
   YOUTUBE_API_KEY: "YOUR_API_KEY_HERE"
 };
